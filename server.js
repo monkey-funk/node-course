8d493258011f252d265c79c70337bfaba2d6df9c
Taylor Funk <taylor.funk@zenimax.com>
2018-08-06 12:08:31 -0400

    adding projects

--- server.js
+++ server.js
@@ -42,6 +42,13 @@ app.get('/', (req, res) => {
   });
 });
 
+app.get('/projects', (req, res) => {
+  res.render('projects.hbs', {
+    pageTitle: 'Projects Page',
+    welcomeMessage: 'These are the projects'
+  });
+});
+
 app.get('/about', (req, res) => {
   res.render('about.hbs', {
     pageTitle: 'About Page'
